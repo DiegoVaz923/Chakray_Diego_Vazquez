4e4245f2ba073072758ee441883c69315180544b
DiegoVaz923 <vazquezsotelodiego@gmail.com>
2026-06-04 13:42:54 -0600

    chore: initialize Spring Boot project

---
+++ src/test/java/org/chakray/diego_vazquez/DiegoVazquezApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chakray.diego_vazquez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiegoVazquezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0dcf0a111dcd7bee9b247ab1d2474acbcd373c49
DiegoVaz923 <vazquezsotelodiego@gmail.com>
2026-06-04 17:53:06 -0600

    feat: implement password masking and Madagascar time zone

---
+++ src/main/java/org/chakray/diego_vazquez/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package org.chakray.diego_vazquez.mapper;
+
+import org.chakray.diego_vazquez.dto.response.UserResponse;
+import org.chakray.diego_vazquez.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class UserMapper {
+    public UserResponse toResponse(User user) {
+
+        ZoneId madagascarZone = ZoneId.of("Indian/Antananarivo");
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+        String formattedDate = user.getCreatedAt().atZone(ZoneId.systemDefault())
+                                .withZoneSameInstant(madagascarZone)
+                                .format(formatter);
+
+        return new UserResponse(user.getId(),user.getEmail(), user.getName(),
+                user.getPhone(), user.getTaxId(), formattedDate, user.getAddresses()
+        );
+    }
+}
